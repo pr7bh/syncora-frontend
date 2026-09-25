d87e36679cd5fab854f437149377bf5a2bb8d75c
Prabhjot Singh <151633829+pr7bh@users.noreply.github.com>
2026-09-25 05:50:57 +0530

    Use environment API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import { getToken } from "../utils/auth";
 
-const API_URL = "http://localhost:8000";
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 
 async function authFetch(url, options = {}) {
